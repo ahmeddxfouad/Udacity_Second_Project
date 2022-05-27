e6f6d564d6499dc32d5823282756ad485a77c4ed
Ahmed Fouad <dx_afl_1997@hotmail.com>
2022-05-27 15:39:40 +0200

    Third Commit

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,10 +1,12 @@
 //your tests
 
+import { response } from 'express';
 import supertest from 'supertest';
 import app from '..';
+import { User, UserStore } from '../models/user';
 
 const test = supertest(app);
-
+let token : string
 describe('Testing Enviroment', () => {
   //Testing Root
   it('Test Root ', async () => {
@@ -20,9 +22,44 @@ describe('Testing Enviroment', () => {
     expect(res.text).toBe('Bad Request');
   });
 
+  it('Test Create User with correct body', async () => {
+
+    const res = await test
+      .post('/users')
+      .send({ 
+        fname:"First",
+        lname:"Last",
+        username:"User",
+        password:"Pass",
+        role:"user" })
+      .set('Accept', 'application/json')
+  
+    expect(res.status).toEqual(200);
+    token = res.body
+    
+  });
+ 
   it('Test Get Specific User Without Authentication Token', async () => {
     const res = await test.get('/users/:id');
     expect(res.status).toBe(401);
+  });
+
+  it('Test Get Specific User With Authentication Token', async () => {
+
+    const res = await test
+      .get('/users/1')
+      .set('Authorization', 'bearer ' + token)
+    expect(res.status).toEqual(200);
+    
+  });
+
+  it('Test Get All Users With Authentication Token', async () => {
+
+    const res = await test
+      .get('/users')
+      .set('Authorization', 'bearer ' + token)
+    expect(res.status).toEqual(200);
+    
   });
 
   it('Create User With a Missing Field/s', async () => {
@@ -41,9 +78,26 @@ describe('Testing Enviroment', () => {
     expect(res.status).toBe(400);
   });
 
+  it('Test Get Specific Product With valid parameter', async () => {
+    const res = await test.get('/products/1');
+    expect(res.status).toBe(200);
+  });
+
   it('Test Create Product Without Authentication Token', async () => {
     const res = await test.post('/products');
     expect(res.status).toBe(401);
+  });
+
+  it('Test Create Product With Authentication Token', async () => {
+    const res = await test
+    .post('/products')
+    .send({ 
+      name:"product1",
+      price:10,
+      category :"food"})
+    .set('Accept', 'application/json')
+    .set('Authorization', 'bearer ' + token)
+    expect(res.status).toBe(200);
   });
 
   //Orders
@@ -52,9 +106,23 @@ describe('Testing Enviroment', () => {
     expect(res.status).toBe(401);
   });
 
+  it('Test Get All Order With Authentication Token', async () => {
+    const res = await test
+    .get('/orders')
+    .set('Authorization', 'bearer ' + token)
+    expect(res.status).toBe(200);
+  });
+
   it('Test Get Specific Order Without Authentication', async () => {
     const res = await test.get('/orders/:id');
     expect(res.status).toBe(401);
+  });
+
+  it('Test Get Specific Order With Authentication', async () => {
+    const res = await test
+    .get('/orders/1')
+    .set('Authorization', 'bearer ' + token)
+    expect(res.status).toBe(200);
   });
 
   it('Test Get Orders for specific User Without Authentication', async () => {
@@ -62,13 +130,31 @@ describe('Testing Enviroment', () => {
     expect(res.status).toBe(401);
   });
 
+  it('Test Get Orders for specific User With Authentication', async () => {
+    const res = await test.get('/users/1/orders')
+    .set('Authorization', 'bearer ' + token)
+    expect(res.status).toBe(200);
+  });
+
   it('Test Add Product to Specific Order Without Authentication', async () => {
     const res = await test.get('/orders/:id');
     expect(res.status).toBe(401);
   });
 
+  it('Test Add Product to Specific Order With Authentication', async () => {
+    const res = await test.get('/orders/1')
+    .set('Authorization', 'bearer ' + token)
+    expect(res.status).toBe(200);
+  });
+
   it('Test Create Order Without Authentication', async () => {
     const res = await test.get('/orders');
     expect(res.status).toBe(401);
+  });
+
+  it('Test Create Order With Authentication', async () => {
+    const res = await test.get('/orders')
+    .set('Authorization', 'bearer ' + token)
+    expect(res.status).toBe(200);
   });
 });
